37372d4305a46077061502fd6e57c5a3508cf5de
hernan0066 <hernan0066@gmail.com>
2022-09-13 17:53:06 -0300

    Add movies controller

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -4,8 +4,46 @@ module.exports ={
     list:(req, res)=>{
         db.Movie.findAll()
         .then((movies)=>{
-            return res.send(movies)
+            return res.render('moviesList', {
+                movies
+            })
         })
         .catch(error => console.log(error))
-    }
+    },
+    detail:(req, res)=>{
+        db.Movie.findByPk(req.params.id)
+        .then((movie)=>{
+            return res.render('moviesDetail', {
+                movie
+            })
+        })
+        .catch(error => console.log(error))
+    },
+    new:(req, res)=>{
+        db.Movie.findAll({
+            order: [
+                ['release_date', 'DESC']
+            ]
+        })
+        .then((movies)=>{
+            return res.render('newestMovies', {
+                movies
+            })
+        })
+        .catch(error => console.log(error))
+    },
+    recomended:(req, res)=>{
+        db.Movie.findAll({
+            order: [
+                ['rating', 'DESC']
+            ],
+            limit: 5 
+        })
+        .then((movies)=>{
+            return res.render('recommendedMovies', {
+                movies
+            })
+        })
+        .catch(error => console.log(error))
+    },
 }
